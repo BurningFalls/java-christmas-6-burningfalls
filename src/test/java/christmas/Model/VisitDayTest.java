2cf50b3c9e0bdf3c22a32900c71e255b89595b28
BurningFalls <sherlocky31415@gmail.com>
2023-11-12 14:52:09 +0900

    style: 방문 날짜 입력값 처리에 ValueSource 도입

--- src/test/java/christmas/Model/VisitDayTest.java
+++ src/test/java/christmas/Model/VisitDayTest.java
@@ -4,7 +4,6 @@ import Model.VisitDay;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,7 +11,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class VisitDayTest {
 
     @DisplayName("방문 날짜 입력값이 숫자가 아니면 예외 처리")
-    @Test
+    @ValueSource(strings = {"3day", "number", "1a5", "abed"})
+    @ParameterizedTest
     void visitDayNotNumber() {
         assertThatThrownBy(() -> new VisitDay("3day"))
                 .isInstanceOf(IllegalArgumentException.class);
